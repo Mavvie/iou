a048c99cc08c7e6722a4c6a4d7ea10299b937fd0
mitchell271 <mitchell271@gmail.com>
2016-03-05 13:04:59 -0500

    Email basics

---
+++ app/mailers/payment_mailer.rb
@@ -0,0 +1,8 @@
+# To be added to payment class
+
+# class PaymentMailer < ApplicationMailer
+#   default from: 'no-reply@iou.com', return_path: 'system@iou.com'
+#   def notification(receiver)
+#     mail(to: receiver.email, subject: "New Payment From #{receiver}", text: "#{receiver} has paid you.")
+#   end
+# end
