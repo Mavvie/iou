2fbd909218586751010ab1d9b3332193c5ef458e
Tyler Boyd <tylerboydblue@gmail.com>
2016-03-04 23:40:55 -0500

    Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :groups
   has_many :sent_payments, class_name: "Payment", foreign_key: :sender_id
   has_many :received_payments, class_name: "Payment", foreign_key: :receiver_id
